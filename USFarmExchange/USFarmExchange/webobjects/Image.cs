9ce9c58a2d42d0118d9940832db1392518196ecc
David Nuckolls <dnuckolls@meditract.com>
2019-07-27 12:30:27 -0400

    Initial commit for website framework

---
+++ USFarmExchange/USFarmExchange/webobjects/Image.cs
@@ -0,0 +1,9 @@
+﻿namespace USFarmExchange {
+  public class Image {
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+    public string ThumbnailUrl { get; set; }
+  }
+}
