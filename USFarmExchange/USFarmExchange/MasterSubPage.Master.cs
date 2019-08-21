38b19dc0ae10619b6e21cf9d8052f6893a37c5e3
David Nuckolls <dnuckolls@meditract.com>
2019-08-21 15:35:03 -0400

    Landing page layout

---
+++ USFarmExchange/USFarmExchange/MasterSubPage.Master.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace USFarmExchange {
+  public partial class MasterSubPage :System.Web.UI.MasterPage {
+    protected void Page_Load(object sender, EventArgs e) {
+
+    }
+  }
+}
--- USFarmExchange/USFarmExchange/MasterSubPage.Master.designer.cs
+++ USFarmExchange/USFarmExchange/MasterSubPage.Master.designer.cs
@@ -10,7 +10,7 @@
 namespace USFarmExchange {
     
     
-    public partial class MasterPage {
+    public partial class MasterSubPage {
         
         /// <summary>
         /// head control.
@@ -31,24 +31,6 @@ namespace USFarmExchange {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// RadPageLayout1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::Telerik.Web.UI.RadPageLayout RadPageLayout1;
-        
-        /// <summary>
-        /// MainNavMenu control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::Telerik.Web.UI.RadMenu MainNavMenu;
-        
-        /// <summary>
         /// MasterLayout control.
         /// </summary>
         /// <remarks>
@@ -56,6 +38,24 @@ namespace USFarmExchange {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::Telerik.Web.UI.RadPageLayout MasterLayout;
+        
+        /// <summary>
+        /// RadMenu1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Telerik.Web.UI.RadMenu RadMenu1;
+        
+        /// <summary>
+        /// RadMenu2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Telerik.Web.UI.RadMenu RadMenu2;
         
         /// <summary>
         /// ContentPlaceHolder1 control.
