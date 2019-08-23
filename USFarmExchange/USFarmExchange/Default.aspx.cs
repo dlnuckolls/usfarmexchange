41350eccb79b1e58f5740eba81d9fe9a33ea84b1
David Nuckolls <dnuckolls@meditract.com>
2019-08-23 13:28:51 -0400

    start working on RSS Feeds

--- USFarmExchange/USFarmExchange/Default.aspx.cs
+++ USFarmExchange/USFarmExchange/Default.aspx.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml;
+using Telerik.Web.UI;
 
 namespace USFarmExchange {
   public partial class Default :System.Web.UI.Page {
     protected void Page_Load(object sender, EventArgs e) {
 
     }
+
   }
 }
