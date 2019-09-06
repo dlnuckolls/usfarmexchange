d9ec7c8c521761a2528a34c1bd4449b80003680b
Jennifer Hobbs <sthobbs269@gmail.com>
2019-09-05 20:32:14 -0400

    Progress Commit - Crop Insurance

--- USFarmExchange/USFarmExchange/areas/crop-insurance.aspx.cs
+++ USFarmExchange/USFarmExchange/areas/crop-insurance.aspx.cs
@@ -7,11 +7,51 @@ using System.Web.UI.WebControls;
 
 namespace USFarmExchange.areas
 {
-    public partial class crop_insurance : System.Web.UI.Page
+    public partial class crop_insurance : BasePage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (IsPostBack) return;
+            Hide_Panels();
+            Display_Panel(UnderstandingCropInsurance);
+            btnPrevious.Enabled = false;
+        }
+
+        protected void btnPrevious_Click(object sender, EventArgs e)
+        {
 
         }
+
+        protected void btnNext_Click(object sender, EventArgs e)
+        {
+            if (UnderstandingCropInsurance.Visible)
+            {
+                Hide_Panels();
+                Display_Panel(CropInsuranceCycle);
+                btnPrevious.Enabled = true;
+                return;
+            }
+            if (CropInsuranceCycle.Visible)
+            {
+                Hide_Panels();
+                Display_Panel(Competition);
+                return;
+            }
+
+        }
+        private void Hide_Panels() {
+            UnderstandingCropInsurance.Visible = false;
+            CropInsuranceCycle.Visible = false;
+            Competition.Visible = false;
+            History.Visible = false;
+            Glossary.Visible = false;
+         }
+
+        private void Display_Panel(Panel panel )
+        {
+            panel.Visible = true;
+        }
+
     }
+
 }
--- USFarmExchange/USFarmExchange/areas/crop-insurance.aspx.designer.cs
+++ USFarmExchange/USFarmExchange/areas/crop-insurance.aspx.designer.cs
@@ -56,5 +56,23 @@ namespace USFarmExchange.areas {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Panel Glossary;
+        
+        /// <summary>
+        /// btnPrevious control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Telerik.Web.UI.RadButton btnPrevious;
+        
+        /// <summary>
+        /// btnNext control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Telerik.Web.UI.RadButton btnNext;
     }
 }
