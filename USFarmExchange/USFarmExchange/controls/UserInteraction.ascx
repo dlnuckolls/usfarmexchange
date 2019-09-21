603db52328d2bb593c82d82ae35007d5fb00273e
David Nuckolls <dlnuckolls@gmail.com>
2019-09-21 10:42:24 -0400

    Fix nav menus for Admins

--- USFarmExchange/USFarmExchange/controls/UserInteraction.ascx
+++ USFarmExchange/USFarmExchange/controls/UserInteraction.ascx
@@ -1,14 +1,16 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="UserInteraction.ascx.cs" Inherits="USFarmExchange.controls.UserInteraction" %>
 <div style="text-align: right;">
   <div class="loginDisplay dashboardLink">
-    <asp:LoginView ID="HeadLoginView" runat="server" EnableViewState="false" >
+    <asp:LoginView ID="HeadLoginView" runat="server" EnableViewState="false">
       <AnonymousTemplate>
-         Welcome back <span class="bold">Guest</span>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<asp:LinkButton ID="LoginLink" CssClass="dashboardLink" runat="server">Log In</asp:LinkButton>
+        Welcome back <span class="bold">Guest</span>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<asp:LinkButton ID="LoginLink" CssClass="dashboardLink" runat="server">Log In</asp:LinkButton>
       </AnonymousTemplate>
       <LoggedInTemplate>
         Welcome back <span class="bold">
           <asp:LoginName ID="HeadLoginName" runat="server" />
-        </span>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;<asp:LoginStatus ID="HeadLoginStatus" runat="server" CssClass="dashboardLink" LogoutAction="Redirect" LogoutText="Log Out" LogoutPageUrl="~/" />
+        </span>&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:LinkButton ID="lbAdminDashboard" runat="server" CssClass="dashboardLink" OnClick="lbAdminDashboard_Click">Admin Dashboard</asp:LinkButton><span runat="server" id="logoutSpacer">&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;</span>
+        <asp:LoginStatus ID="HeadLoginStatus" runat="server" CssClass="dashboardLink" LogoutAction="Redirect" LogoutText="Log Out" LogoutPageUrl="~/" />
       </LoggedInTemplate>
     </asp:LoginView>
   </div>
--- USFarmExchange/USFarmExchange/controls/UserInteraction.ascx.cs
+++ USFarmExchange/USFarmExchange/controls/UserInteraction.ascx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using YAF.Types.Extensions;
 using YAF.Utils;
@@ -13,6 +14,16 @@ namespace USFarmExchange.controls {
     protected void Page_Load(object sender, EventArgs e) {
       var loginLink = this.HeadLoginView.FindControlAs<LinkButton>("LoginLink");
       if(loginLink != null) { loginLink.PostBackUrl = "~/forum/forum.aspx?g=login&ReturnUrl={0}".FormatWith(this.GetReturnUrl()); }
+      if(!HttpContext.Current.User.Identity.IsNullOrEmpty()) {
+        SessionInfo.CurrentUser.UserName = HttpContext.Current.User.Identity.Name;
+        SessionInfo.CurrentUser.IsAuthenticated = HttpContext.Current.User.Identity.IsAuthenticated;
+        SessionInfo.CurrentUser.IsAdmin = YAF.Core.RoleMembershipHelper.IsUserInRole(SessionInfo.CurrentUser.UserName, "Administrators");
+        SessionInfo.CurrentUser.IsSuperAdmin = YAF.Core.RoleMembershipHelper.IsUserInRole(SessionInfo.CurrentUser.UserName, "Administrators");
+        var spacer = HeadLoginView.FindControlAs<HtmlGenericControl>("logoutSpacer");
+        if(!spacer.IsNullOrEmpty()) spacer.Visible = SessionInfo.IsAdmin;
+        var adminMenu = HeadLoginView.FindControlAs<LinkButton>("lbAdminDashboard");
+        if(!adminMenu.IsNullOrEmpty()) adminMenu.Visible = (SessionInfo.IsAdmin && SessionInfo.IsAuthenticated);
+      }
     }
 
     protected string GetReturnUrl() {
@@ -21,6 +32,10 @@ namespace USFarmExchange.controls {
               HttpContext.Current.Request.QueryString.GetFirstOrDefault("ReturnUrl").IsSet()
                   ? General.GetSafeRawUrl(HttpContext.Current.Request.QueryString.GetFirstOrDefault("ReturnUrl"))
                   : General.GetSafeRawUrl());
+    }
+
+    protected void lbAdminDashboard_Click(object sender, EventArgs e) {
+      Response.Redirect("~/admin/AdminHome.aspx");
     }
   }
 }
