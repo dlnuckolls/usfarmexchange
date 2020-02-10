00c57bd95444c592d6e725a417094e781d6b5913
David Nuckolls <dlnuckolls@gmail.com>
2020-02-10 11:43:00 -0500

    Changes for #71 -- Adding contact form and submission code

--- USFarmExchange/USFarmExchange/areas/ContactUs.aspx.cs
+++ USFarmExchange/USFarmExchange/areas/ContactUs.aspx.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Telerik.Web.UI;
 
 namespace USFarmExchange.areas {
   public partial class ContactUs : BasePage {
@@ -12,6 +14,28 @@ namespace USFarmExchange.areas {
       SessionInfo.CurrentPage = PageNames.Contact;
       TitleTag.Text = SessionInfo.DisplayCurrentPage;
       ContactUsHomeContent.Text = SessionInfo.PageContent(PageContentBlocks.ContactUsHomeContent);
+
+      RadCaptcha1.CaptchaImage.LineNoise = Telerik.Web.UI.CaptchaLineNoiseLevel.Low;
+      RadCaptcha1.CaptchaImage.FontWarp = Telerik.Web.UI.CaptchaFontWarpFactor.Low;
+      RadCaptcha1.MinTimeout = 15;
+      RadCaptcha1.ProtectionMode = RadCaptcha.ProtectionStrategies.Captcha;
+      RadCaptcha1.CaptchaImage.TextLength = 6;
+      RadCaptcha1.CaptchaImage.FontFamily = "Verdana";
+      RadCaptcha1.CaptchaImage.TextColor = System.Drawing.Color.FromName("Green");
+      RadCaptcha1.CaptchaImage.BackgroundColor = System.Drawing.Color.FromName("White");
+      RadCaptcha1.CaptchaImage.BackgroundNoise = Telerik.Web.UI.CaptchaBackgroundNoiseLevel.Low;
+    }
+
+    protected void SendMessage_Click(object sender, EventArgs e) {
+      if (!Page.IsValid) return;
+      var msg = new MailMessage {
+        IsBodyHtml = false,
+        From = new MailAddress(ContactEmail.Text.Trim(), ContactName.Text.Trim()),
+        Subject = ContactSubject.Text.Trim(),
+        Body = ContactMessage.Text.Trim()
+      };
+      msg.To.Add(new MailAddress("contactus@usfarmexchange.com", "USFarmExchange Contact Service"));
+      SessionInfo.SendContactEmail(ref msg);
     }
   }
 }
--- USFarmExchange/USFarmExchange/areas/ContactUs.aspx.designer.cs
+++ USFarmExchange/USFarmExchange/areas/ContactUs.aspx.designer.cs
@@ -22,6 +22,87 @@ namespace USFarmExchange.areas {
     protected global::System.Web.UI.WebControls.Literal TitleTag;
 
     /// <summary>
+    /// RadAjaxPanel1 control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::Telerik.Web.UI.RadAjaxPanel RadAjaxPanel1;
+
+    /// <summary>
+    /// ContactName control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::Telerik.Web.UI.RadTextBox ContactName;
+
+    /// <summary>
+    /// ContactNameValidator control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.RequiredFieldValidator ContactNameValidator;
+
+    /// <summary>
+    /// ContactEmail control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::Telerik.Web.UI.RadTextBox ContactEmail;
+
+    /// <summary>
+    /// ContactEmailValidator control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::System.Web.UI.WebControls.RequiredFieldValidator ContactEmailValidator;
+
+    /// <summary>
+    /// ContactSubject control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::Telerik.Web.UI.RadTextBox ContactSubject;
+
+    /// <summary>
+    /// ContactMessage control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::Telerik.Web.UI.RadTextBox ContactMessage;
+
+    /// <summary>
+    /// RadCaptcha1 control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::Telerik.Web.UI.RadCaptcha RadCaptcha1;
+
+    /// <summary>
+    /// SendMessage control.
+    /// </summary>
+    /// <remarks>
+    /// Auto-generated field.
+    /// To modify move field declaration from designer file to code-behind file.
+    /// </remarks>
+    protected global::Telerik.Web.UI.RadButton SendMessage;
+
+    /// <summary>
     /// ContactUsHomeContent control.
     /// </summary>
     /// <remarks>
